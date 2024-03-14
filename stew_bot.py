884612eb98774db2e692ca2bb7f09d1063ad14bc
Femi Makinde <femimakinde@icloud.com>
2024-03-13 19:17:20 -0500

    Added multiple subs, urls, etc.

--- stew_bot.py
+++ stew_bot.py
@@ -1,21 +1,29 @@
 import praw
+import os
 
+# Documentation: https://praw.readthedocs.io/en/stable/
 # Inspiration: https://praw.readthedocs.io/en/latest/tutorials/reply_bot.html
 
 # Bot Name: u/theLadled
+def load_ladle()->praw.Reddit:
+        reddit= praw.Reddit("LadleBot")
+        return reddit
 
-with open("info.txt") as f:
-    auth_info = []
-    for line in f:
-        auth_info.append(line)
+def main():
+    reddit = load_ladle()
 
-    reddit = praw.Reddit(
-        client_id =auth_info[0],
-        client_secret =auth_info[1],
-        password = auth_info[2],
-        user_agent=auth_info[3],
-        username=auth_info[4],
-    )
+    print(reddit.user.me())
 
-print(reddit.user.me())
+    sub_list = ["piano","guitar","classicalmusic","learnprogramming","computerscience","math"]
 
+    search = ""
+    for sub in sub_list[:-1]:
+        search += sub + "+"
+    search += sub_list[-1]
+
+
+    for submission in reddit.subreddit(search).hot(limit=25):
+        print(f"{submission.title} {submission.upvote_ratio} {submission.url}")
+
+if __name__ == "__main__":
+    main()
