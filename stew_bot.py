e9d3d899f872cb71e8e514b5a146d6f1ab53e6b7
Femi Makinde <femimakinde@icloud.com>
2024-03-22 10:37:17 -0500

    Updated project directory

--- stew_bot.py
+++ stew_bot.py
@@ -9,21 +9,32 @@ def load_ladle()->praw.Reddit:
         reddit= praw.Reddit("LadleBot")
         return reddit
 
-def main():
+def load_posts(amount: int):
+    if amount < 0 or amount > 100:
+         amount = 25 # Default Cutoff
     reddit = load_ladle()
 
-    print(reddit.user.me())
-
-    sub_list = ["piano","guitar","classicalmusic","learnprogramming","computerscience","math"]
+    sub_list = ["piano","guitar","classicalmusic","learnprogramming","computerscience","machinelearning","math"]
 
     search = ""
     for sub in sub_list[:-1]:
         search += sub + "+"
     search += sub_list[-1]
 
+    posts = []
+    for submission in reddit.subreddit(search).hot(limit=amount):
+        info = f"{submission.title} {submission.upvote_ratio} {submission.url} {submission.score}"  
+        posts.append((f"{submission.title}",submission.upvote_ratio,submission.score))
+    
+    posts = sorted(posts,key=lambda x : x[1] * x[2],reverse=True)
 
-    for submission in reddit.subreddit(search).hot(limit=25):
-        print(f"{submission.title} {submission.upvote_ratio} {submission.url}")
+    return posts
+
+def main():
+    items = load_posts(30)
+    for post in items:
+         print(post)
+     
 
 if __name__ == "__main__":
     main()
